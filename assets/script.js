48f81cb4b36c3bcfc8c5c64450124f88f70f3a88
kitkatyourmom <sarahlridings@gmail.com>
2023-04-28 09:35:02 -0600

    link first pop-up javascript

--- assets/script.js
+++ assets/script.js
@@ -11,4 +11,34 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", windowPrompt);
+
+// all code above provided by course
+
+// first pop up when clicking on generate password
+function windowPrompt() {
+  if (confirm("Let's create a new password! Please answer a few prompts to set the parameters for your new password.")) {
+    //numberSelect();
+  } else {
+    // window closes
+  }
+}
+
+
+// function numberSelect() {
+//   let number = prompt("Please enter how many characters your new password will have. Please write a whole number between 8 - 128.", "14");
+//   if (isNaN || number < 8 || number > 128) {
+
+//   } else if () {
+
+//   } else {
+
+//   }
+// }
+
+
+// passwordText
+
+// final = paswordText.value
+
+// Math.floor(Math.random() * 10);
