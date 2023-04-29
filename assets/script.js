e4b26a9f62dfdff1b239a14e093e576a9cce14f9
kitkatyourmom <sarahlridings@gmail.com>
2023-04-29 15:47:49 -0600

    create noted function outline

--- assets/script.js
+++ assets/script.js
@@ -18,23 +18,82 @@ generateBtn.addEventListener("click", windowPrompt);
 // first pop up when clicking on generate password
 function windowPrompt() {
   if (confirm("Let's create a new password! Please answer a few prompts to set the parameters for your new password.")) {
-    //numberSelect();
+    //lenSelect();
   } else {
     // window closes
   }
 }
 
 
-// function numberSelect() {
+// function lenSelect() {
 //   let number = prompt("Please enter how many characters your new password will have. Please write a whole number between 8 - 128.", "14");
 //   if (isNaN || number < 8 || number > 128) {
-
+  // 
 //   } else if () {
 
 //   } else {
 
 //   }
 // }
+
+// function retryLen() {
+  // if (confirm("Please select a number between 8 and 128. If you would like to cancel, press \"cancel\".")) {
+    // lenSelect()
+  //} else {
+    // window closes
+  //}
+//}
+
+function upCase() {
+  if (confirm("Would you like uppercase letters in your password?")) {
+    // lowCase ()
+  } else {
+    // lowCase ()
+  }
+}
+
+function lowCase() {
+  if (confirm("Would you like lowercase letters in your password?")) {
+    // number()
+  } else {
+    // needRetry()
+  }
+}
+
+// functuion needRetryC() {
+  // if (lowCase || upCase) {
+    // number()
+  //} else {
+    // retry()
+  //}
+//}
+
+// function retryC() {
+  //if (confirm("Please select \"okay\" to either uppercase or lowercase characters. Select \"okay\" to go reselect uppercase and lowercase. If you would like to cancel, press \"cancel\".")) {
+    // upCase()
+  //} else {
+    // close window
+  //}
+//}
+
+function number() {
+  if (confirm("Would you like numbers in your password?")) {
+    // specials()
+  } else {
+    // specials()
+  }
+}
+
+function specials() {
+  if (confirm("Would you like special characters in your password?")) {
+    // lowcase ()
+  } else {
+    // lowcase ()
+  }
+}
+
+
+
 
 
 // passwordText
