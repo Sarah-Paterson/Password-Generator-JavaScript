08b5a3616b36f37b4fb63f605b25a640b0d5d380
kitkatyourmom <sarahlridings@gmail.com>
2023-05-05 23:25:47 -0600

    output password

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", windowPrompt);
@@ -18,9 +18,9 @@ generateBtn.addEventListener("click", windowPrompt);
 // array of options for the passsword and empty arrays for password
 let uperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let specials = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/'", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-let newPass = [];
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let specials = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", "-", ".", "/'", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+let password = [];
 let availChar = [];
 
 // let usrLen;
@@ -200,17 +200,31 @@ function randomize (availChar)
 randomize (availChar);
 console.log(availChar);
 
-//This code is contributed by rohitsingh07052.
+//This code above is contributed by rohitsingh07052.
 
-function slicePass(usrLen) {
-  if(availChar >= usrLen) {
-    return availChar.slice(0, usrLen);
-  }
-  return[];
+// for(var i = arr.length; i >= 0; i--) {
+//   if(arr[i] == cls) {
+//       alert(arr[i - 1]);
+//       var splCout = arr.splice(arr[i - 1], 1);
+//       alert(splCout);
+//   }
+// }
+
+for(let i=0; i < usrLen; i++) {
+  password = availChar.splice(1, i);
 }
 
-slicePass()
-console.log(newPass);
+
+// Write password to the #password input
+function writePassword() {
+  // var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password.join("");
+
+}
+
+writePassword();
  
 }
 
